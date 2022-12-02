8934da6190475b8bf2e39e36c02d39ed498d8d8c
stradivarius96 <matthew.pavan@gmail.com>
2022-12-02 17:14:48 +0000

    day2 part1

--- day01.js
+++ day01.js
@@ -4,7 +4,7 @@ let arr = fs.readFileSync('day01-input.txt').toString().split("\n");
 // loop through, only store highest total
 let mostCalories = 0
 let currentCalories = 0
-for (let i = 1; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
     if (arr[i].length > 0){
         currentCalories += parseInt(arr[i])
     }else{
@@ -19,7 +19,7 @@ console.log("Part 1 Result: " + mostCalories)
 // shifted to array of all totals from original solution
 currentCalories = 0
 let totals = []
-for (let i = 1; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
     if (arr[i].length > 0){
         currentCalories += parseInt(arr[i])
     }else{
--- day02.js
+++ day02.js
@@ -5,39 +5,42 @@ fs.readFileSync('day02-input.txt').toString().split("\n").forEach(row => {arr.pu
 let usValue = {'X': 1, 'Y': 2, 'Z' : 3}
 let themValue = {'A': 1, 'B': 2, 'C' : 3}
 
-
-for (let i = 1; i < arr.length; i++) {
+let gameScores = []
+for (let i = 0; i < arr.length; i++) {
     let us = usValue[arr[i][1]]
     let them = themValue[arr[i][0]]
 
     let game = 0
-    switch ((us-them)%3) {
-        case: 0
+    switch (us-them) {
+        case 0:
             game = 3
             break;
-        case: 1
+        case 1:
+        case -2:
             game = 6
             break;
-        case: 2
+        case -1:
+        case 2:
             game = 0
             break;
     }
-    let score = us + game
+    gameScores.push(us + game)
+    console.log(`${gameScores.length}: ${us}  ${them} ${game} ${((us-them)%3)} ${us + game}`)
 }
-console.log("Part 1 Result: " + mostCalories)
+console.log("Part 1 Result: " + gameScores.reduce((a,b)=>a+b, 0))
 
-// shifted to array of all totals from original solution
-currentCalories = 0
-let totals = []
-for (let i = 1; i < arr.length; i++) {
-    if (arr[i].length > 0){
-        currentCalories += parseInt(arr[i])
-    }else{
-        totals.push(currentCalories)
-        currentCalories = 0
-    }
-}
-totals = totals.sort((a,b) => b-a)
+// // shifted to array of all totals from original solution
+// currentCalories = 0
+// let totals = []
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i].length > 0){
+//         currentCalories += parseInt(arr[i])
+//     }else{
+//         totals.push(currentCalories)
+//         currentCalories = 0
+//     }
+// }
+// totals = totals.sort((a,b) => b-a)
 
 
-console.log("Part 2 Result: " + (totals[0] + totals[1] + totals[2]))
+// console.log("Part 2 Result: " + (totals[0] + totals[1] + totals[2]))
